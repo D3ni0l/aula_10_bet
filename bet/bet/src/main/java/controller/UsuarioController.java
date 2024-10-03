4e23491793aafdfff237eb431e930a257a52c2e0
D3ni0l <d3n74l.dos.estudos@gmail.com>
2024-10-03 17:08:58 -0300

    Add files via upload

---
+++ bet/bet/src/main/java/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brenda.bet.model.UsuarioModel;
+
+import dto.UsuarioDTO;
+import repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("api")
+public class UsuarioController {
+
+	@Autowired
+	UsuarioRepository repository;
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<?> salvarUsuario(@RequestBody UsuarioDTO user) {
+		UsuarioModel usuario = new UsuarioModel(user);
+		repository.save(usuario);
+		return  ResponseEntity.ok("usuario cadastrado");
+		
+	}
+}
---
+++ bet/bet/src/main/java/dto/UsuarioDTO.java
@@ -0,0 +1,8 @@
+package dto;
+
+import java.util.UUID;
+
+public record UsuarioDTO
+(UUID id,String cpf,String aniver,String email,String senha) {
+
+}
